76d44a1ec83f311c9a118573cde290dc4fd02f41
s1uth <handtaein@gmail.com>
2024-11-20 11:10:49 +0900

    create domain

--- src/main/java/domain/Article.java
+++ src/main/java/domain/Article.java
@@ -1,4 +1,40 @@
 package domain;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
